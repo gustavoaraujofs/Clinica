a9678c4a3d66750ee3bf2b13f3af46aa8912a8f2
20gustavo <gustavoaraujofs2526@gmail.com>
2022-10-24 17:43:20 -0300

    Iniciando o main

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from unicodedata import name
+import os
+from clinica import Clinica
 
+def main():
+    c = Clinica()
+    c.login()
+    op_menu_principal = 1
+    while(op_menu_principal != 0):
+        os.system("cls")
+        op_menu_principal = c.menu_principal()
+    os.system("cls")
 
 if __name__=="__main__":
-    print("Olá Mundo")
+    dados = {}
+    agenda = {}
+    main()
